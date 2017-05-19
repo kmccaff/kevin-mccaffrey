9fa6d8a552cbf46117c31c046a385fbf35d7962b
Kevin McCaffrey <kevinericmccaffrey@gmail.com>
2017-05-18 21:11:05 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -75,7 +75,7 @@ particlesJS('particles-js',
     "events": {
       "onhover": {
         "enable": true,
-        "mode": "grab"
+        "mode": "repulse"
       },
       "onclick": {
         "enable": true,
